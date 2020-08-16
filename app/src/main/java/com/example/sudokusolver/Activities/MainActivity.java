4e03b309e6a5f1bcee397a5d3c20febcb4ee699d
mrmnt <mantas.vadopalas@gmail.com>
2020-08-16 18:31:27 +0300

    Initial commit

---
+++ app/src/main/java/com/example/sudokusolver/Activities/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.sudokusolver.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.sudokusolver.Activities.ManualMode;
+import com.example.sudokusolver.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpButtons(this);
+    }
+
+    private void setUpButtons(final Context mainActivityContext){
+        findViewById(R.id.manualModeBtn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Creates an Intent, from this activity, to the ManualMode activity
+                Intent intent = new Intent(mainActivityContext, ManualMode.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/sudokusolver/Views/SudokuGridView.java
@@ -0,0 +1,14 @@
+package com.example.sudokusolver.Views;
+
+import android.content.Context;
+import android.view.View;
+
+public class SudokuGridView extends View {
+
+
+    public SudokuGridView(Context context) {
+        super(context);
+    }
+
+
+}
