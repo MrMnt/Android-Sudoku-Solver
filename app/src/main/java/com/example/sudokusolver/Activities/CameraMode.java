c85bb5b5cea230992292b6a25fe8bbb0b52405d4
mrmnt <mantas.vadopalas@gmail.com>
2020-09-05 17:18:44 +0300

    Working camera Preview

--- app/src/main/java/com/example/sudokusolver/Activities/CameraMode.java
+++ app/src/main/java/com/example/sudokusolver/Activities/CameraMode.java
@@ -2,15 +2,104 @@ package com.example.sudokusolver.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.example.sudokusolver.R;
 
-public class CameraMode extends AppCompatActivity {
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera_mode);
-    }
+public class CameraMode extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+        private static final String TAG = "ActivityCamera";
+
+        // For displaying the camera
+        JavaCameraView mOpenCvCameraView;
+
+        int[][] temp = new int[9][9];
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_camera_mode);
+
+            setLandscapeAndStuff();
+            initializeCamera();
+            initializeProcessing();
+        }
+
+        @Override
+        public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+            return inputFrame.rgba();
+        }
+
+        private void initializeProcessing(){
+
+        }
+
+        private void initializeCamera(){
+            mOpenCvCameraView = findViewById(R.id.myCameraView);
+            mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+            mOpenCvCameraView.setCvCameraViewListener(CameraMode.this);
+            //mOpenCvCameraView.setMaxFrameSize(1920, 1080);
+        }
+        private void setLandscapeAndStuff(){
+            getSupportActionBar().hide();
+            View decorView = getWindow().getDecorView();
+            int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+            decorView.setSystemUiVisibility(uiOptions);
+            //set orientation
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            //permanent full screen
+            getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+
+        private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+                switch (status) {
+                    case LoaderCallbackInterface.SUCCESS: {
+                        mOpenCvCameraView.enableView();
+                    }
+                    break;
+                    default: {
+                        super.onManagerConnected(status);
+                    }
+                    break;
+                }
+            }
+        };
+        static { OpenCVLoader.initDebug(); }
+        @Override
+        public void onResume()
+        {
+            super.onResume();
+            if (!OpenCVLoader.initDebug()) {
+                Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+                OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+            } else {
+                Log.d(TAG, "OpenCV library found inside package. Using it!");
+                mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+            }
+        }
+
+        @Override
+        public void onCameraViewStarted(int width, int height) {
+
+        }
+
+        @Override
+        public void onCameraViewStopped() {
+
+        }
 }
